82322035ef1197b6f9c0e0fbc09501ead3567cc5
Zachary Spector <zjspector@gmail.com>
2014-04-16 15:34:29 -0400

    Add initialize methods to Person and Bank classes

--- banking.rb
+++ banking.rb
@@ -1,12 +1,37 @@
 class Person
-	def initialize(name, amount)
+	attr_reader :name
+	attr_accessor :pocket_change
+	attr_accessor :bank_account
+	attr_accessor :total
+	
+	def initialize(name, pocket_change)
 		@name = name
-		@amount = amount
+		@pocket_change = pocket_change
+		@bank_account = 0
+		@total = pocket_change + bank_account
+		puts "Hi, #{@name}. You have #{@pocket_change}."
 	end
 end
 
 class Bank
 	def initialize(bank_name)
 		@bank_name = bank_name
+		puts "#{@bank_name} was just created."
+	end
+
+	def open_account(person)
+		@account_holder = person
+		@balance = 0
+		puts "#{person.name}, thanks for opening an account at #{@bank_name}!"
+	end
+
+	def withdraw(amount)
+
+	end
+
+	def deposit(amount)
+	end
+
+	def transfer(amount)
 	end
 end
---
+++ test.rb
@@ -0,0 +1,8 @@
+chase = Bank.new("JP Morgan Chase")
+wells_fargo = Bank.new("Wells Fargo")
+me = Person.new("Shehzan", 500)
+friend1 = Person.new("John", 1000)
+chase.open_account(me)
+chase.open_account(friend1)
+wells_fargo.open_account(me)
+wells_fargo.open_account(friend1)
